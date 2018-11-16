10d2d62568663a891b475b1c0927ec4a221ceedb
Aeolus1364 <thetesseractsshadow@gmail.com>
2018-11-15 21:55:09 -0500

    Added get, set, and print commands. Command collapsing structure now works! Commands can resolve infinitely recursively. Once the final argument for a command is entered it instantly collapses before the next step. A chain of commands will all collapse at the last argument and execute the first (priority one) command.

--- commands.py
+++ commands.py
@@ -11,13 +11,43 @@ class Command:
         self.num_args -= 1
 
     def complete(self):
+        if self.num_args < 0:
+            print("Error: number of arguments less than zero")
         return self.num_args == 0
 
 
-class Set(Command):
+class Set(Command):  # sets variable value in var stream
     def __init__(self, priority):
         super().__init__(2, priority, "var")
+        # arg 0: var name
+        # arg 1: var value
 
     def evaluate(self, var):
         var[self.args[0]] = self.args[1]
         return var
+
+
+class Get(Command):  # gets variable value from var stream
+    def __init__(self, priority):
+        super().__init__(1, priority, "value")
+        # arg 0: var name
+
+    def evaluate(self,  var):
+        return var[self.args[0]]  # returns arg value from var dict
+
+
+class Print(Command):
+    def __init__(self, priority):
+        super().__init__(1, priority, "none")
+        # arg 0: prints value
+
+    def evaluate(self):
+        print(self.args[0])
+
+
+class Default(Command):
+    def __init__(self, priority):
+        super().__init__(0, priority, "")
+
+    def evaluate(self):
+        pass
--- interpreter.py
+++ interpreter.py
@@ -13,7 +13,8 @@ class Interpreter:
         self.cmds = []  # highest priority command receives arguments
 
         self.list_cmd = open("cmd_list.txt", "r").read().split()  # list of all commands
-
+        self.holder = None  # holds returned values until the next command activates
+        self.resolve = True
         self.halt = False
 
     def load(self, file):
@@ -39,31 +40,45 @@ class Interpreter:
         if packtype == "cmd":
             self.priority += 1  # new command gets 1 higher priority
             self.cmds.append(self.get_cmd(packet)(self.priority))  # adds new active command
-            active = self.cmds[self.priority - 1]
 
         elif packtype == "val":
             active.argument(packet)  # sends argument to priority command
 
-        if active.complete():  # on command completion
-            type = active.return_type  # get return type
-            if type == "var":  # passes in requested data to modify
-                self.var = active.evaluate(self.var)
-            elif type == "stream":
-                self.stream = active.evaluate(self.stream)
-            elif type == "current":
-                self.current = active.evaluate(self.current)
-            elif type == "none":
-                active.evaluate()
-            else:  # error if return type not there or doesn't match
-                self.raise_error(f"'{active.__class__.__name__}' has an invalid return type: '{active.return_type}'")
+        while self.resolve:
+            active = self.cmds[self.priority - 1]
+            self.resolve = False
 
-            del self.cmds[self.priority - 1]  # remove command from active
-            self.priority -= 1  # drop priority
+            if self.holder:
+                active.argument(self.holder)
+                self.holder = None
+
+            if active.complete():  # on command completion
+                type = active.return_type  # get return type
+                # passes in requested data to modify
+                if type == "var":  # changing var stream
+                    self.var = active.evaluate(self.var)
+                elif type == "stream":  # changing packet stream
+                    self.stream = active.evaluate(self.stream)
+                elif type == "current":  # changing current packet
+                    self.current = active.evaluate(self.current)
+                elif type == "value":  # returns argument for next command
+                    self.holder = active.evaluate(self.var)
+                    self.resolve = True
+                elif type == "none":
+                    active.evaluate()
+                else:  # error if return type not there or doesn't match
+                    self.raise_error(f"'{active.__class__.__name__}' has an invalid return type: '{active.return_type}'")
+
+                del self.cmds[self.priority - 1]  # remove command from active
+                self.priority -= 1  # drop priority
+            # print("repeat")
+        self.resolve = True
 
         if self.current == len(self.stream) - 1:  # stops on last command
             self.halt = True
 
-        print(self.current, self.var)
+        # print(self.current, self.var, self.holder)
+        # print(self.current, self.priority, active.__class__.__name__, active.args)
 
         self.current += 1
 
@@ -83,11 +98,15 @@ class Interpreter:
     def get_cmd(self, packet):
         if packet == "set":
             return commands.Set
+        elif packet == "get":
+            return commands.Get
+        elif packet == "print":
+            return commands.Print
         else:
             self.raise_error(f"{packet} is an invalid command")
 
     def raise_error(self, error):
-        print(error)
+        print("Error: " + error)
         self.halt = True
 
 
